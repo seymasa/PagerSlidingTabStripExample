734d72de9d076189d1e8128387bed1a7cd9571ce
Your Name <you@example.com>
2019-04-14 23:41:44 +0300

    example

---
+++ app/src/main/java/com/example/radyotv/PagerAdapter.java
@@ -0,0 +1,31 @@
+package com.example.radyotv;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class PagerAdapter extends FragmentPagerAdapter {
+    private final String[] TITLES = {"Radyo", "Tv"};
+
+    PagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return TITLES[position];
+    }
+
+    @Override
+    public int getCount() {
+        return TITLES.length;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if(position == 0)
+            return RadyoFragment.newInstance();
+        else
+            return TvFragment.newInstance();
+    }
+}
---
+++ app/src/main/java/com/example/radyotv/TvFragment.java
@@ -0,0 +1,52 @@
+package com.example.radyotv;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TvFragment extends Fragment {
+    private OnFragmentInteractionListener mListener;
+
+    public TvFragment(){}
+
+
+    public static TvFragment newInstance() {
+        TvFragment fragment = new TvFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tv, container, false);
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+
+}
